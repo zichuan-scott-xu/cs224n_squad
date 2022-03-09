0463e5a4525292fdaa9bcff04598dfc58641dcc9
Scott_Xu <scottxu@stanford.edu>
2022-03-08 20:53:37 -0800

    change default batch size

--- args.py
+++ args.py
@@ -223,7 +223,7 @@ def add_train_test_args(parser):
                         help='Base directory for saving information.')
     parser.add_argument('--batch_size',
                         type=int,
-                        default=64,
+                        default=32,
                         help='Batch size per GPU. Scales automatically when \
                               multiple GPUs are available.')
     parser.add_argument('--use_squad_v2',
