18b2ccc17e3464a9c3cc402f48e6be69cc53c079
Ubuntu <hanson@squad-machine.bjh5y1pfatdu5a2a1w5xncfttf.jx.internal.cloudapp.net>
2022-03-10 04:18:44 +0000

    modify training

--- train_qanet.py
+++ train_qanet.py
@@ -18,7 +18,7 @@ import math
 from args import get_train_args
 from collections import OrderedDict
 from json import dumps
-from qanet import QANet
+from qanet2 import QANet
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from ujson import load as json_load
